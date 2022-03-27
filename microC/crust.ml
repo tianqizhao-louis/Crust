9ad883c57d2b15d8644b4ea528bfd0653dc7f8b3
Annie Song <afsong2018@gmail.com>
2022-03-27 17:47:33 -0400

    add default build target

--- microC/crust.ml
+++ microC/crust.ml
@@ -25,4 +25,5 @@ let () =
     match !action with
       Ast     -> ()
     | Sast    -> print_string (Sast.string_of_sprogram sast)
-    | LLVM_IR -> print_string (Llvm.string_of_llmodule (Irgen.translate sast))
+    | LLVM_IR -> ()
+    (* | LLVM_IR -> print_string (Llvm.string_of_llmodule (Irgen.translate sast)) *)
