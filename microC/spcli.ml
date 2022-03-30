addefe08c358081663cf0c38660b84fa40cdc453
tianqizhao-louis <louiszhao0517@gmail.com>
2022-03-30 00:52:23 -0400

    main cleanup

--- microC/spcli.ml
+++
@@ -1,8 +0,0 @@
-(* Command-line interface for testing scanner and parser *)
-
-open Ast
-
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Crustparser.program Scanner.token lexbuf in
-  print_endline (string_of_program program)
