301eb2bfc6db3e36677f2d0cfabe7bc07ae57f4e
Frank Zhang <frankzhang@dyn-160-39-210-70.dyn.columbia.edu>
2022-05-11 11:58:40 -0400

    test

--- src/helper_in_c.c
+++ src/helper_in_c.c
@@ -26,7 +26,7 @@ char* awk_line_f(char* text, char* pattern, char* decision) {
     char yes[15];
     strcpy(yes, "y");
     char no[15];
-    strcpy(yes, "n");
+    strcpy(no, "n");
     int count = 1;
 
 
@@ -35,7 +35,7 @@ char* awk_line_f(char* text, char* pattern, char* decision) {
     char *line, *str, *tofree;
     tofree = str = strdup(text);
 
-    if (strcmp(yes, "y")){
+    if (strcmp(decision, "y")){
         char buffer[1024];
         while ((line = strsep(&str, "\n"))) {
             if (strstr(line, pattern)) {
