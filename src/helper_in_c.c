4ac1eae76775456a3aedc8d150b96b7a3a8a43f8
Frank Zhang <frankzhang@dyn-160-39-210-70.dyn.columbia.edu>
2022-05-11 16:49:41 -0400

    test

--- src/helper_in_c.c
+++ src/helper_in_c.c
@@ -190,22 +190,30 @@ char* awk_col_f(char* text, char* pattern, int col_num) {
     }
 
     while ((line = strsep(&str, "\n"))) {
-        printf("%s/n", line);
-        int tmp_col_count = 1;
-        line = strsep(&str, "\n");
-        char * token = strtok(line, " ");
-        while( token != NULL ) {
-            if (tmp_col_count == col_num) {
-                line = token;
-            }
-            token = strtok(NULL, " ");
-            tmp_col_count++;
+        if (strstr(line, pattern)) {
+            strcpy(res+res_len, line);
+            res_len += strlen(line);
+            res[res_len] = '\n';
+            res_len++;
         }
-        strcpy(res+res_len, line);
-        res_len += strlen(line);
-        res[res_len] = '\n';
-        res_len++;
     }
+    // while ((line = strsep(&str, "\n"))) {
+    //     printf("%s/n", line);
+    //     int tmp_col_count = 1;
+    //     line = strsep(&str, "\n");
+    //     char * token = strtok(line, " ");
+    //     while( token != NULL ) {
+    //         if (tmp_col_count == col_num) {
+    //             line = token;
+    //         }
+    //         token = strtok(NULL, " ");
+    //         tmp_col_count++;
+    //     }
+    //     strcpy(res+res_len, line);
+    //     res_len += strlen(line);
+    //     res[res_len] = '\n';
+    //     res_len++;
+    // }
     return res;
 }
 
