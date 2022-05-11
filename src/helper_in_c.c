bab51097c85caf79aafd41d9820c9133fcefcbd7
Frank Zhang <frankzhang@dyn-160-39-210-70.dyn.columbia.edu>
2022-05-11 15:57:11 -0400

    test

--- src/helper_in_c.c
+++ src/helper_in_c.c
@@ -185,19 +185,26 @@ char* awk_col_f(char* text, char* pattern, int col_num) {
         printf("Invalid column number");
         exit(0);
     }
-    printf("pass");
 
-    // while ((line = strsep(&str, "\n"))) {
-    //     if (strstr(line, pattern)) {
-    //         strcpy(res+res_len, line);
-    //         res_len += strlen(line);
-    //         res[res_len] = '\n';
-    //         res_len++;
-    //     }
-    // }
+    while ((line = strsep(&str, "\n"))) {
+        int tmp_col_count = 1;
+        line = strsep(&str, "\n");
+        char * token = strtok(line, " ");
+        while( token != NULL ) {
+            if (tmp_col_count == col_num) {
+                line = token;
+            }
+            token = strtok(NULL, " ");
+            tmp_col_count++;
+        }
+        strcpy(res+res_len, line);
+        res_len += strlen(line);
+        res[res_len] = '\n';
+        res_len++;
+    }
 
     // return res;
-    return 0;
+    return res;
 }
 
 
