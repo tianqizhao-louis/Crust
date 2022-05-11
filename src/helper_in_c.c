791ae6988411ae5211d2536b2c3fa03ef5a5f795
Frank Zhang <frankzhang@franks-mbp.myfiosgateway.com>
2022-05-11 00:43:50 -0400

    test

--- src/helper_in_c.c
+++ src/helper_in_c.c
@@ -25,8 +25,8 @@ char* awk_f(char* text, char* pattern) {
 char* awk_line_f(char* text, char* pattern, char* print_row) {
     char buffer[50];
     // sprintf(buffer, "i got here")
-    // char yes = "y"
-    // char no = "n"
+    char yes = "y"
+    char no = "n"
 
     if (strcmp(print_row, &yes)){
         sprintf(buffer, "true");
