ece8ae1624ce11b2d907658ec55a3e6c0a7e057b
Frank Zhang <frankzhang@dyn-160-39-211-211.dyn.columbia.edu>
2022-05-12 13:30:14 -0400

    test

--- src/runtest.sh
+++ src/runtest.sh
@@ -3,14 +3,12 @@ git pull
 make clean
 ocamlbuild -pkgs llvm crust.native
 
-printf "test awk_line\n"
+printf "test awk_line\n\n\n"
 ./crust.native -l ../test/awk_test/awk_line.mc > ../test/awk_test/awk_line.out
 make all
 chmod +x run.sh
 ./run.sh ../test/awk_test/awk_line.mc
 
-printf "test awk_line_range\n"
+printf "test awk_line_range\n\n\n"
 ./crust.native -l ../test/awk_test/awk_line_range.mc > ../test/awk_test/awk_line_range.out
-make all
-chmod +x run.sh
 ./run.sh ../test/awk_test/awk_line_range.mc
