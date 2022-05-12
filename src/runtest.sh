a726a4ac4cd11924ab49b3154dbb1e6affaade0e
Frank Zhang <frankzhang@dyn-160-39-211-211.dyn.columbia.edu>
2022-05-12 13:37:11 -0400

    test

--- src/runtest.sh
+++ src/runtest.sh
@@ -5,19 +5,23 @@ ocamlbuild -pkgs llvm crust.native
 
 make all
 chmod +x run.sh
-printf "test awk_line\n\n\n"
+printf "\n\n\ntest awk_line"
 ./crust.native -l ../test/awk_test/awk_line.mc > ../test/awk_test/awk_line.out
 ./run.sh ../test/awk_test/awk_line.mc
 
-printf "test awk_line_range\n\n\n"
+printf "\n\n\ntest awk_line_range"
 ./crust.native -l ../test/awk_test/awk_line_range.mc > ../test/awk_test/awk_line_range.out
 ./run.sh ../test/awk_test/awk_line_range.mc
 
 
-printf "test awk_line_range_start\n\n\n"
+printf "\n\n\ntest awk_line_range_start"
 ./crust.native -l ../test/awk_test/awk_line_range_start.mc > ../test/awk_test/awk_line_range_start.out
 ./run.sh ../test/awk_test/awk_line_range_start.mc
 
-printf "test awk_line_range_end\n\n\n"
+printf "\n\n\ntest awk_line_range_end"
 ./crust.native -l ../test/awk_test/awk_line_range_end.mc > ../test/awk_test/awk_line_range_end.out
 ./run.sh ../test/awk_test/awk_line_range_end.mc
+
+printf "\n\n\ntest awk_col"
+./crust.native -l ../test/awk_test/awk_col.mc > ../test/awk_test/awk_col.out
+./run.sh ../test/awk_test/awk_col.mc
